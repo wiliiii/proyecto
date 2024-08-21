66eab86820f221021c7f6771080e69829cdebade
wiliiii <wilicho2014@gmail.com>
2024-08-21 11:34:02 -0400

    cambio antes de ingresar barra de busqueda (bootstrap en paginacion)

--- resources/views/datper/index.blade.php
+++ resources/views/datper/index.blade.php
@@ -11,20 +11,11 @@
                 <div class="card">
                     <div class="card-header">
                         <div style="display: flex; justify-content: space-between; align-items: center;">
-
-                            <form action="{{ route('datpers.index') }}" method="GET">
-                                <div class="input-group">
-                                    <input type="text" name="search" class="form-control" placeholder="Buscar datos....." value="{{ request('search') }}">
-                                    <span class="input-group-btn">
-                                        <button type="submit" class="btn btn-primary">Buscar</button>
-                                    </span>
-                                </div>
-                            </form>
                             
 
                              <div class="float-right">
                                 <a href="{{ route('datpers.create') }}" class="btn btn-primary btn-sm float-right"  data-placement="left">
-                                  {{ __('Create New') }}
+                                  {{ __('Crear registro') }}
                                 </a>
                               </div>
                         </div>
@@ -42,21 +33,17 @@
                                     <tr>
                                         <th>No</th>
                                         
-									<th >No Historia</th>
-									<th >Ci</th>
-									<th >Nombre1</th>
-									<th >Appaterno</th>
-									<th >Apmaterno</th>
+									<th >Cedula</th>
+									<th >Nombre</th>
+									<th >Apellido Paterno</th>
+									<th >Apellido materno</th>
 									<th >Sexo</th>
-									<th >Est Civil</th>
-									<th >Fec Nac</th>
+									<th >nacimiento</th>
 									<th >Direccion</th>
-									<th >Fono</th>
-									<th >Fono2</th>
 									<th >Celular</th>
 									<th >Ciudad</th>
 
-                                        <th></th>
+                                        <th>Acciones</th>
                                     </tr>
                                 </thead>
                                 <tbody>
@@ -64,27 +51,23 @@
                                         <tr>
                                             <td>{{ ++$i }}</td>
                                             
-										<td >{{ $datper->no_historia }}</td>
 										<td >{{ $datper->CI }}</td>
 										<td >{{ $datper->nombre1 }}</td>
 										<td >{{ $datper->appaterno }}</td>
 										<td >{{ $datper->apmaterno }}</td>
 										<td >{{ $datper->sexo }}</td>
-										<td >{{ $datper->est_civil }}</td>
 										<td >{{ $datper->fec_nac }}</td>
 										<td >{{ $datper->direccion }}</td>
-										<td >{{ $datper->fono }}</td>
-										<td >{{ $datper->fono2 }}</td>
 										<td >{{ $datper->celular }}</td>
 										<td >{{ $datper->ciudad }}</td>
 
                                             <td>
                                                 <form action="{{ route('datpers.destroy', $datper->id) }}" method="POST">
-                                                    <a class="btn btn-sm btn-primary " href="{{ route('datpers.show', $datper->id) }}"><i class="fa fa-fw fa-eye"></i> {{ __('Show') }}</a>
-                                                    <a class="btn btn-sm btn-success" href="{{ route('datpers.edit', $datper->id) }}"><i class="fa fa-fw fa-edit"></i> {{ __('Edit') }}</a>
+                                                    <a class="btn btn-sm btn-primary " href="{{ route('datpers.show', $datper->id) }}"><i class="fa fa-fw fa-eye"></i> {{ __('Mostrar') }}</a>
+                                                    <a class="btn btn-sm btn-success" href="{{ route('datpers.edit', $datper->id) }}"><i class="fa fa-fw fa-edit"></i> {{ __('Editar') }}</a>
                                                     @csrf
                                                     @method('DELETE')
-                                                    <button type="submit" class="btn btn-danger btn-sm" onclick="event.preventDefault(); confirm('Are you sure to delete?') ? this.closest('form').submit() : false;"><i class="fa fa-fw fa-trash"></i> {{ __('Delete') }}</button>
+                                                    <button type="submit" class="btn btn-danger btn-sm" onclick="event.preventDefault(); confirm('Are you sure to delete?') ? this.closest('form').submit() : false;"><i class="fa fa-fw fa-trash"></i> {{ __('Borrar') }}</button>
                                                 </form>
                                             </td>
                                         </tr>
